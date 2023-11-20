747e016d257b57b07042435918c4794daf4d1f1d
Koduhwan <rhenghks5167@naver.com>
2023-11-20 21:13:05 +0900

    day-1

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 using namespace std;
 
+template <typename T>
+T TestFunc(T a){
+    cout<<"maegaebyeonsu a: "<< a << endl;
+    return a;
+}
 
 int main(int argc, char* argv[]) {
 
-    int *pData = new int;
-    int *pnewData = new int(10);
-    *pData = 5;
-    cout<<*pData<<endl;
-    cout<<*pnewData<<endl;
-
-    delete pData;
-    delete pnewData;
+    cout<<"int\t" << TestFunc(3)<<endl;
+    cout<<"double\t" << TestFunc(3.3)<<endl;
+    cout<<"char\t" << TestFunc('A')<<endl;
+    cout<<"char*\t" << TestFunc("TestString")<<endl;
 
     return 0;
 }
