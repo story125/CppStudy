6301fdaf97b0c1cc93c501a4195e20526f036aa1
Koduhwan <rhenghks5167@naver.com>
2023-11-20 15:14:05 +0900

    day-1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main(int argc, char* argv[]) {
+
+    int *pData = new int;
+    int *pnewData = new int(10);
+    *pData = 5;
+    cout<<*pData<<endl;
+    cout<<*pnewData<<endl;
+
+    delete pData;
+    delete pnewData;
+
     return 0;
 }
